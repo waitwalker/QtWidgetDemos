27cca49b71e56bc345c674ba895ba95fa7954070
waitwalker <waitwalker@163.com>
2021-06-28 11:17:16 +0800

    居上显示

--- base64/mainwindow.cpp
+++ base64/mainwindow.cpp
@@ -15,9 +15,25 @@ MainWindow::MainWindow(QWidget *parent)
     QPushButton *button1 = new QPushButton("button1");
     QPushButton *button2 = new QPushButton("button2");
 
-    QVBoxLayout *vLayout = new QVBoxLayout;
-    vLayout->addWidget(button1);
-    vLayout->addWidget(button2);
+    QPushButton *button3 = new QPushButton("button3");
+    QPushButton *button4 = new QPushButton("button4");
+
+
+    QVBoxLayout *vLayout = new QVBoxLayout(widget);
+    QHBoxLayout *hLayout = new QHBoxLayout;
+
+    //void QLayout::setContentsMargins(int left, int top, int right, int bottom)
+    vLayout->setContentsMargins(100,30,200,100);
+
+    // 设置控件之间的间距
+    //vLayout->setSpacing(10);
+    hLayout->addWidget(button1);
+    hLayout->addWidget(button2);
+    vLayout->addLayout(hLayout);
+    vLayout->addWidget(button3);
+    vLayout->addWidget(button4);
+    //  居上显示 添加一个弹框
+    vLayout->addStretch();
 
     widget->setLayout(vLayout);
     setCentralWidget(widget);
