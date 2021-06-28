8f89fbb37ab5bb0e57d91286c3aeffbf90f862f4
waitwalker <waitwalker@163.com>
2021-06-28 11:36:15 +0800

    设置伸缩系数

--- base64/mainwindow.cpp
+++ base64/mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "ui_mainwindow.h"
 #include <QPushButton>
 #include <QVBoxLayout>
+#include <QLineEdit>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -17,21 +18,30 @@ MainWindow::MainWindow(QWidget *parent)
 
     QPushButton *button3 = new QPushButton("button3");
     QPushButton *button4 = new QPushButton("button4");
-
+    QLineEdit *lineEdit = new QLineEdit;
 
     QVBoxLayout *vLayout = new QVBoxLayout(widget);
     QHBoxLayout *hLayout = new QHBoxLayout;
+    QHBoxLayout *lineContainer = new QHBoxLayout;
+    QHBoxLayout *buttonContainer = new QHBoxLayout;
+
+
+    buttonContainer->addWidget(button1);
+    buttonContainer->addWidget(button2);
+    lineContainer->addWidget(lineEdit);
+    hLayout->addLayout(lineContainer);
+    hLayout->addLayout(buttonContainer);
+    hLayout->setStretchFactor(lineContainer,1);
+    hLayout->setStretchFactor(buttonContainer,1);
 
     //void QLayout::setContentsMargins(int left, int top, int right, int bottom)
-    vLayout->setContentsMargins(100,30,200,100);
+    vLayout->setContentsMargins(30,30,30,100);
 
     // 设置控件之间的间距
     //vLayout->setSpacing(10);
     // 居中显示 上下分别添加一个弹簧
-    // 居下显示
-    vLayout->addStretch();
-    hLayout->addWidget(button1);
-    hLayout->addWidget(button2);
+    // 居下显示 在上面添加一个弹簧
+    //vLayout->addStretch();
     vLayout->addLayout(hLayout);
     vLayout->addWidget(button3);
     vLayout->addWidget(button4);
