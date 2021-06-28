3031a96080c1ec88adecfe2f22be0632c1fee6d9
waitwalker <waitwalker@163.com>
2021-06-28 11:20:56 +0800

    居下显示

--- base64/mainwindow.cpp
+++ base64/mainwindow.cpp
@@ -27,6 +27,8 @@ MainWindow::MainWindow(QWidget *parent)
 
     // 设置控件之间的间距
     //vLayout->setSpacing(10);
+    // 居下显示
+    vLayout->addStretch();
     hLayout->addWidget(button1);
     hLayout->addWidget(button2);
     vLayout->addLayout(hLayout);
