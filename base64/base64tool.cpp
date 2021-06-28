5e3df736c4c1afd9167db19d2fd3a78835e66d28
waitwalker <waitwalker@163.com>
2021-06-28 14:02:51 +0800

    base64 工具类初始化

---
+++ base64/base64tool.cpp
@@ -0,0 +1,6 @@
+#include "base64tool.h"
+
+Base64Tool::Base64Tool()
+{
+
+}
---
+++ base64/base64tool.h
@@ -0,0 +1,11 @@
+#ifndef BASE64TOOL_H
+#define BASE64TOOL_H
+
+
+class Base64Tool
+{
+public:
+    Base64Tool();
+};
+
+#endif // BASE64TOOL_H
