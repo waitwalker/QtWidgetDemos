e9cb5b0778194421f02d8dc87c6bfb2e09953d43
waitwalker <waitwalker@163.com>
2021-06-28 10:21:26 +0800

    在QMainWindow主窗口中添加布局

--- base64/mainwindow.cpp
+++ base64/mainwindow.cpp
@@ -1,11 +1,27 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPushButton>
+#include <QVBoxLayout>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // 1)创建一个widget
+    QWidget *widget = new QWidget;
+
+    QPushButton *button1 = new QPushButton("button1");
+    QPushButton *button2 = new QPushButton("button2");
+
+    QVBoxLayout *vLayout = new QVBoxLayout;
+    vLayout->addWidget(button1);
+    vLayout->addWidget(button2);
+
+    widget->setLayout(vLayout);
+    setCentralWidget(widget);
+
 }
 
 MainWindow::~MainWindow()
