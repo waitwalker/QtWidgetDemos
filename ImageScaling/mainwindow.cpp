c59aa56742f1e30e1390dac35977e8f68cd03f17
waitwalker <waitwalker@163.com>
2021-07-30 09:02:53 +0800

    添加URL按钮&取消按钮事件监听添加

--- ImageScaling/mainwindow.cpp
+++ ImageScaling/mainwindow.cpp
@@ -6,6 +6,31 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setWindowTitle(tr("Image downloading and scaling example"));
+    resize(800,600);
+
+    addUrlsButton = new QPushButton(tr("Add URLs"));
+
+
+    cancelButton = new QPushButton("Cancel");
+    cancelButton->setEnabled(false);
+
+    QHBoxLayout *buttonLayout = new QHBoxLayout();
+    buttonLayout->addWidget(addUrlsButton);
+    buttonLayout->addWidget(cancelButton);
+    buttonLayout->addStretch();
+
+    statusBar = new QStatusBar();
+
+    imagesLayout = new QGridLayout();
+
+    mainLayout = new QVBoxLayout();
+    mainLayout->addLayout(buttonLayout);
+    mainLayout->addLayout(imagesLayout);
+    mainLayout->addStretch();
+    mainLayout->addWidget(statusBar);
+    setLayout(mainLayout);
+
 }
 
 MainWindow::~MainWindow()
--- ImageScaling/mainwindow.h
+++ ImageScaling/mainwindow.h
@@ -1,7 +1,14 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "DownloadDialog.h"
+
+#include <QFuture>
+#include <QLabel>
 #include <QMainWindow>
+#include <QNetworkAccessManager>
+#include <QPushButton>
+#include <QVBoxLayout>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +22,32 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    // 初始化布局
+    void initLayout(qsizetype count);
+
+    // 下载
+    QFuture<QByteArray> download(const QList<QUrl> &urls);
+
+    QList<QImage> scaled() const;
+
+    void updateStatus(const QString &msg);
+    void showImages(const QList<QImage> &images);
+    void abortDownload();
+
 private:
     Ui::MainWindow *ui;
+
+    QPushButton *addUrlsButton;
+    QPushButton *cancelButton;
+    QVBoxLayout *mainLayout;
+    QList<QLabel *> labels;
+    QGridLayout *imagesLayout;
+    QStatusBar *statusBar;
+    DownloadDialog *downloadDialog;
+
+    QNetworkAccessManager qnam;
+    QList<QSharedPointer<QNetworkReply>> replies;
+    QFuture<QByteArray> downloadFuture;
+
 };
 #endif // MAINWINDOW_H
