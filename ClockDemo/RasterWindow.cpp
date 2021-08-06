72fdf95b3f4f0205da9fcac9225a641597d5f404
waitwalker <waitwalker@163.com>
2021-08-06 15:48:36 +0800

    时钟demo添加

--- ClockDemo/RasterWindow.cpp
+++ ClockDemo/RasterWindow.cpp
@@ -1,13 +1,14 @@
 #include "RasterWindow.h"
 
-RasterWindow::RasterWindow(QWindow *parent)
+RasterWindow::RasterWindow(QWindow *parent):QWindow(parent),m_backingStore(new QBackingStore(this))
 {
-
+    setGeometry(100,100,300,200);
 }
 
 void RasterWindow::render(QPainter *painter)
 {
-
+    // 绘制文字
+    painter->drawText(QRectF(0,0,width(),height()),Qt::AlignCenter,QStringLiteral("QWindow"));
 }
 
 RasterWindow::~RasterWindow()
@@ -17,26 +18,45 @@ RasterWindow::~RasterWindow()
 
 void RasterWindow::renderLater()
 {
-
+    requestUpdate();
 }
 
 void RasterWindow::renderNow()
 {
+    if (!isExposed()) {
+        return;
+    }
+    QRect rect(0,0,width(),height());
+    m_backingStore->beginPaint(rect);
 
+    QPaintDevice *device = m_backingStore->paintDevice();
+    QPainter painter(device);
+
+    painter.fillRect(0,0,width(), height(),QGradient::NightFade);
+    render(&painter);
+    painter.end();
+
+    m_backingStore->endPaint();
+    m_backingStore->flush(rect);
 }
 
 bool RasterWindow::event(QEvent *event)
 {
-
+    if (event->type() == QEvent::UpdateRequest) {
+        renderNow();
+        return true;
+    }
+    return QWindow::event(event);
 }
 
 void RasterWindow::resizeEvent(QResizeEvent *event)
 {
-
+    m_backingStore->resize(event->size());
 }
 
 void RasterWindow::exposeEvent(QExposeEvent *event)
 {
-
+    if (isExposed())
+        renderNow();
 }
 
--- ClockDemo/main.cpp
+++ ClockDemo/main.cpp
@@ -1,11 +1,93 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QtGui>
+#include "RasterWindow.h"
+
+class AnalogClockWindow: public RasterWindow {
+public:
+    AnalogClockWindow();
+protected:
+    void timerEvent(QTimerEvent *) override;
+    void render(QPainter *p) override;
+private:
+    int m_timerId;
+};
+
+AnalogClockWindow::AnalogClockWindow() {
+    setTitle("Clock");
+    resize(200,200);
+    m_timerId = startTimer(1000);
+}
+
+void AnalogClockWindow::timerEvent(QTimerEvent * event) {
+    if (event->timerId() == m_timerId) {
+        renderLater();
+    }
+}
+
+void AnalogClockWindow::render(QPainter *p) {
+    static const QPoint hourHand[3] = {
+        QPoint(7,8),
+        QPoint(-7,8),
+        QPoint(0,-40),
+    };
+
+    static const QPoint minuteHand[3] = {
+        QPoint(7,8),
+        QPoint(-7,8),
+        QPoint(0,-70),
+    };
+
+    QColor hourColor(127, 0, 127);
+    QColor minuteColor(0, 127, 127, 191);
+
+    p->setRenderHint(QPainter::Antialiasing);
+    p->translate(width() / 2, height() / 2);
+
+    int side = qMin(width(), height());
+    p->scale(side / 200.0, side / 200.0);
+
+    p->setPen(Qt::NoPen);
+    p->setBrush(hourColor);
+
+    QTime time = QTime::currentTime();
+
+    p->save();
+    p->rotate(30.0 * ((time.hour() + time.minute() / 60.0)));
+    p->drawConvexPolygon(hourHand, 3);
+    p->restore();
+
+    p->setPen(hourColor);
+
+    for (int i =0 ; i < 12 ;++i ) {
+        p->drawLine(88, 0,96,0);
+        p->rotate(30.0);
+    }
+
+    p->setPen(Qt::NoPen);
+    p->setBrush(minuteColor);
+
+    p->save();
+    p->rotate(6.0 * (time.minute() + time.second() / 60.0));
+    p->drawConvexPolygon(minuteHand, 3);
+    p->restore();
+
+    p->setPen(minuteColor);
+    for (int j = 0; j < 60 ; ++j) {
+        p->drawLine(92,0,96,0);
+        p->rotate(6.0);
+    }
+
+
+}
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    AnalogClockWindow clock;
+    clock.show();
     return a.exec();
 }
